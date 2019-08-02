1fc8213cf0795325a4395bf39cce9352440e4d73
Eric <cheryleric0828@gmail.com>
2019-08-02 14:15:05 +1000

    add association between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,14 @@ def new
   @article = Article.new
 end
 def create
-  @article = Article.new(article_params)
-  if @article.save
-   flash[:success] = "Article was successfully created"
-   redirect_to article_path(@article)
-  else
-   render 'new'
-  end
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+     flash[:notice] = "Article was successfully created"
+     redirect_to article_path(@article)
+    else
+     render 'new'
+    end
 end
 def show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_034934) do
+ActiveRecord::Schema.define(version: 2019_08_02_040340) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
